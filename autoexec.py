62159eda0d26c857e73724fc72388f150541aa54
J <35180978+jesusjorge@users.noreply.github.com>
2025-07-22 12:37:23 -0700

    Update autoexec.py
    The reason for this, is on order to skip the CDN Cache Delay.
    
    - Clean and understandable code.
    
    - Uses urllib.request.urlopen to fetch the contents of a file over https
    
    - Uses GitHub API to view the contents of a file. We need to decode such contents from Base64

--- autoexec.py
+++ autoexec.py
@@ -1,14 +1,18 @@
 import base64
 import json
 import urllib.request
+
 class R1:
+    
     @classmethod
     def GetHttpText(cls,path):
         with urllib.request.urlopen(path) as response:
             return response.read().decode()
+            
     @classmethod
     def GithubFile(cls,owner,repo,path):
         response = json.loads(cls.GetHttpText(f"https://api.github.com/repos/{owner}/{repo}/contents/{path}"))
         b64content = response["content"]
         return base64.b64decode(b64content).decode('utf-8')
+        
 exec(R1.GithubFile("jesusjorge","pysite","index.py"), globals(), locals())

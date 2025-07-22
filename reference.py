08d5890d524ad56b6eef9a90d547e60d929fded4
J <35180978+jesusjorge@users.noreply.github.com>
2025-07-22 11:14:24 -0700

    Update and rename autoexecaaaa.py to reference.py

--- reference.py
+++ reference.py
@@ -2,7 +2,7 @@
 # Date:     2025-07-22 10:58
 # Notes:    Single line bootstraps the python script from this site
 
-exec(__import__('requests').get("https://jesusjorge.github.io/reference.py").text)
+exec(__import__('requests').get("https://jesusjorge.github.io/autoexec.py").text)
 
 
 

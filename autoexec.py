ae3fe83bb6890ddee13da6fd57330d5a76b8acc7
J <35180978+jesusjorge@users.noreply.github.com>
2025-07-22 10:59:09 -0700

    Update autoexec.py

--- autoexec.py
+++ autoexec.py
@@ -1,3 +1,7 @@
+# Revision: Final
+# Date:     2025-07-22 10:58
+# Notes:    Single line bootstraps the python script from this site
+
 exec(__import__('requests').get("https://jesusjorge.github.io/reference.py").text)
 
 

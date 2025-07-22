efafccfbb0de58bf274c3a3c392a9ce920e27412
J <35180978+jesusjorge@users.noreply.github.com>
2025-07-22 11:15:38 -0700

    Update and rename reference.py to bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -2,10 +2,8 @@
 # Date:     2025-07-22 10:58
 # Notes:    Single line bootstraps the python script from this site
 #           If you want to run it from console:
-# python -c "exec(__import__('requests').get('https://jesusjorge.github.io/reference.py').text)"
+# python -c "exec(__import__('requests').get('https://jesusjorge.github.io/autoexec.py').text)"
 
 exec(__import__('requests').get("https://jesusjorge.github.io/autoexec.py").text)
-
-
 
 

ddbd317033ad6c8268a36a3262db086cd6ba2fe5
J <35180978+jesusjorge@users.noreply.github.com>
2025-07-22 11:14:57 -0700

    Update reference.py

--- reference.py
+++ reference.py
@@ -1,6 +1,8 @@
 # Revision: Final
 # Date:     2025-07-22 10:58
 # Notes:    Single line bootstraps the python script from this site
+#           If you want to run it from console:
+# python -c "exec(__import__('requests').get('https://jesusjorge.github.io/reference.py').text)"
 
 exec(__import__('requests').get("https://jesusjorge.github.io/autoexec.py").text)
 

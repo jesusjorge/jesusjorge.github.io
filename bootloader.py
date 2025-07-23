6837c565cf4ca9adf3a852291c014d5974aea3e0
J <35180978+jesusjorge@users.noreply.github.com>
2025-07-22 21:05:19 -0700

    Update bootloader.py

--- bootloader.py
+++ bootloader.py
@@ -2,9 +2,18 @@ import base64
 import json
 import urllib.request
 
-tRequest = urllib.request.urlopen(f"https://api.github.com/repos/jesusjorge/pysite/contents/bootinit.py")
-tResponse = tRequest.read()
-tJson = json.loads(tResponse)
-tContent = tJson["content"]
-tDecoded = base64.b64decode(tContent)
-exec(tDecoded)
+try:
+  tRequest = urllib.request.urlopen(f"https://api.github.com/repos/jesusjorge/pysite/contents/bootinit.py")
+  tResponse = tRequest.read()
+  tJson = json.loads(tResponse)
+  tContent = tJson["content"]
+  tDecoded = base64.b64decode(tContent)
+  exec(tDecoded)
+except urllib.error.HTTPError as e:
+  if e.code == 403:
+    print("⚠️ GitHub API limit reached. Falling back to raw content.")
+    tRequest = urllib.request.urlopen(f"https://raw.githubusercontent.com/jesusjorge/pysite/refs/heads/main/bootinit.py")
+    tResponse = tRequest.read()
+    exec(tResponse)
+  else:
+      raise

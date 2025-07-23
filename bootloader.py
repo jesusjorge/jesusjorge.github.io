6a2bc259abc7ad10fda6fa59e56e848014b19263
J <35180978+jesusjorge@users.noreply.github.com>
2025-07-22 21:33:30 -0700

    Update bootloader.py

--- bootloader.py
+++ bootloader.py
@@ -9,12 +9,13 @@ try:
   tJson = json.loads(tResponse)
   tContent = tJson["content"]
   tDecoded = base64.b64decode(tContent)
+  print("Retrieving from https://api.github.com/")
   exec(tDecoded)
 except urllib.error.HTTPError as e:
   if e.code == 403: #GitHub API rate limit exceeded. Don't panic, just use CDN.
-    print("Retrieving from https://raw.githubusercontent.com/")
     tRequest = urllib.request.urlopen(f"https://raw.githubusercontent.com/jesusjorge/pysite/main/bootinit.py")
     tResponse = tRequest.read()
+    print("Retrieving from https://raw.githubusercontent.com/")
     exec(tResponse)
   else:
       raise

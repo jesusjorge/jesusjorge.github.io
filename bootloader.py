b13a999fdf979a696fce1c6f4b03d8c4c0deb2f1
J <35180978+jesusjorge@users.noreply.github.com>
2025-07-22 20:34:32 -0700

    Update bootloader.py

--- bootloader.py
+++ bootloader.py
@@ -1,4 +1,7 @@
-import base64, json, urllib.request
+import base64
+import json
+import urllib.request
+
 tResponse = urllib.request.urlopen(f"https://api.github.com/repos/jesusjorge/pysite/contents/bootinit.py").read()
 tJson = json.loads(tResponse)
 tContent = tJson["content"]

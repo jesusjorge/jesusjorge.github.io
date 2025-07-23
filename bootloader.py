0ba2afab19a598305a796c4d4197c276cab830dc
J <35180978+jesusjorge@users.noreply.github.com>
2025-07-22 19:58:02 -0700

    Update bootloader.py

--- bootloader.py
+++ bootloader.py
@@ -1,18 +1,4 @@
 import base64
 import json
 import urllib.request
-
-class R1:
-    
-    @classmethod
-    def GetHttpText(cls,path):
-        with urllib.request.urlopen(path) as response:
-            return response.read().decode()
-            
-    @classmethod
-    def GithubFile(cls,owner,repo,path):
-        response = json.loads(cls.GetHttpText(f"https://api.github.com/repos/{owner}/{repo}/contents/{path}"))
-        b64content = response["content"]
-        return base64.b64decode(b64content).decode('utf-8')
-        
-exec(R1.GithubFile("jesusjorge","pysite","bootinit.py"), globals(), locals())
+exec(base64.b64decode(json.loads(urllib.request.urlopen(f"https://api.github.com/repos/jesusjorge/pysite/contents/bootinit.py").read())["content"]))
